58b946412b0a03954afa1f5d75c45c209b8ab929
Jack Dockerty <jdockerty19@gmail.com>
2022-11-27 19:32:51 +0000

    add pod termination message for 'OOMKilled' into tmp file

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
+
+const (
+    // Set in the Deployment manifest.
+	terminationMessagePath = "/tmp/oomed-pod.log"
+)
 
 func main() {
+	f, err := os.Create(terminationMessagePath)
+	if err != nil {
+		log.Fatalf("unable to create %s: %s", terminationMessagePath, err)
+	}
+
+	_, err = f.WriteString("OOMKilled")
+	if err != nil {
+		log.Fatalf("unable to write to %s: %s", terminationMessagePath, err)
+	}
+	f.Close()
+
 	os.Exit(137)
 }
